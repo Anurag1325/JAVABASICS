3f3dd0154372088999e931235300dcb72e525cf3
Anurag chaturvedi <117295370+Babypanda1303@users.noreply.github.com>
2023-05-01 23:04:06 +0530

    Update java_pratice.java

--- java_pratice.java
+++ java_pratice.java
@@ -183,6 +183,19 @@ public class anurag{
     }
 			
     }
+	static void greeting(){
+	Scanner sc=new Scannner(System.in);
+	System.out.println("enter your name:");
+	String name=sc.nextInt();
+	System.out.println("enter your age:");
+	int age=sc.nextInt();
+	if(age>18){
+  		System.out.println("eligible for vacination");
+	}else if (age>18 && age>50){
+  		System.out.println("booster is must");
+	}else{
+  		System.out.println("not eligible for vacination");
+}
 		
 		
   }
