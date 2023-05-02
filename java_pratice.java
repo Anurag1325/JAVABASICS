eb8f4eb65f541abf855ea96e271e4f5c2aa50ebc
Anurag chaturvedi <117295370+Babypanda1303@users.noreply.github.com>
2023-05-02 16:04:50 +0530

    Update java_pratice.java

--- java_pratice.java
+++ java_pratice.java
@@ -253,6 +253,16 @@ public class anurag{
 		System.out.println("year zero does not exist ");	
 		
 		}
+		{//java program sum of digits of a number
+			long n,sum;
+		System.out.println("Enter a number:");
+		n=sc.nextLong();
+		for(sum=0;n!=0;n/=10) {
+			sum+=n%10;
+		}
+		System.out.println("Sum of digit of a number is num: "+sum);
+		}
+	
   }
 }
 
